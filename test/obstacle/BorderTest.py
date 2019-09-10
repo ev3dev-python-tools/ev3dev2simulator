2448cc96974d187be19ea907e64ece0abe5b333d
sam <samjansen16@gmaill.com>
2019-09-10 16:12:17 +0200

    Initialized unit test framework

---
+++ test/obstacle/BorderTest.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class BorderTest(unittest.TestCase):
+
+    def test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test/util/UtilTest.py
@@ -0,0 +1,24 @@
+import unittest
+
+from util.Util import pythagoras, get_circle_points
+
+
+class UtilTest(unittest.TestCase):
+
+    def test_get_circle_points_base(self):
+        points = get_circle_points(100, 100, 10)
+
+        self.assertEqual(len(points), 33)
+
+    def test_get_circle_points_diff(self):
+        points = get_circle_points(100, 100, 10, 64)
+
+        self.assertEqual(len(points), 65)
+
+    def test_pythagoras(self):
+        result = pythagoras(2, 3)
+        self.assertAlmostEqual(result, 3.606, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
