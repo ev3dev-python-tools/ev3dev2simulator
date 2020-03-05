72949119afc3a7b775b61936d05fcf20fb276fe9
Niels Okker <n.okker@student.science.ru.nl>
2020-03-05 12:13:09 +0100

    added original functionality back such as checks on invalid files

--- test/ev3dev2/simulator/test_sound.py
+++ test/ev3dev2/simulator/test_sound.py
@@ -14,68 +14,64 @@ from ev3dev2.sound import Sound
 
 
 class SoundTest(unittest.TestCase):
-    # def test_beep(self):
-    #     spkr = Sound()
-    #     spkr.beep(play_type=1)
-    #
-    # def test_play_tone(self):
-    #     spkr = Sound()
-    #     spkr.play_tone(500, duration=0.3, volume=50, play_type=2)
-    #     spkr.play_tone(1500, duration=0.3, volume=50, play_type=0)
+    def test_beep(self):
+        spkr = Sound()
+        spkr.beep(play_type=1)
+
+    def test_play_tone(self):
+        spkr = Sound()
+        spkr.play_tone(500, duration=0.3, volume=50, play_type=2)
+        spkr.play_tone(1500, duration=0.3, volume=50, play_type=0)
+
+    def test_tone(self):
+        spkr = Sound()
+        spkr.tone([
+            (392, 350, 100), (392, 350), (392,), ()
+        ])
+        print(clientSocketMock.method_calls)
+
+    def test_play_note(self):
+        spkr = Sound()
+        spkr.play_note("C4", 0.5)
+        spkr.play_note("D4", 0.5)
+        spkr.play_note("E4", 0.5)
+        spkr.play_note("C4", 0.5)
+        spkr.play_note("C4", 0.5)
+        spkr.play_note("D4", 0.5)
+        spkr.play_note("E4", 0.5)
+        spkr.play_note("C4", 0.5)
+        spkr.play_note("E4", 0.5)
+        spkr.play_note("F4", 0.5)
+        spkr.play_note("G4", 0.5)
 
 
-    # def test_tone(self):
-    #     spkr = Sound()
-    #     spkr.tone([
-    #         (392, 350, 100), (392, 350), (392,), ()
-    #     ])
-    #     print(clientSocketMock.method_calls)
-    #
-    # def test_play_note(self):
-    #     spkr = Sound()
-    #     spkr.play_note("C4", 0.5)
-    #     spkr.play_note("D4", 0.5)
-    #     spkr.play_note("E4", 0.5)
-    #     spkr.play_note("C4", 0.5)
-    #     spkr.play_note("C4", 0.5)
-    #     spkr.play_note("D4", 0.5)
-    #     spkr.play_note("E4", 0.5)
-    #     spkr.play_note("C4", 0.5)
-    #     spkr.play_note("E4", 0.5)
-    #     spkr.play_note("F4", 0.5)
-    #     spkr.play_note("G4", 0.5)
+    def test_play_song(self):
+        spkr = Sound()
+        spkr.play_song((
+            ('D4', 'e3'),  # intro anacrouse
+            ('D4', 'e3'),
+            ('D4', 'e3'),
+            ('G4', 'h'),  # meas 1
+            ('D5', 'h'),
+            ('C5', 'e3'),  # meas 2
+            ('B4', 'e3'),
+            ('A4', 'e3'),
+            ('G5', 'h'),
+            ('D5', 'q'),
+            ('C5', 'e3'),  # meas 3
+            ('B4', 'e3'),
+            ('A4', 'e3'),
+            ('G5', 'h'),
+            ('D5', 'q'),
+            ('C5', 'e3'),  # meas 4
+            ('B4', 'e3'),
+            ('C5', 'e3'),
+            ('A4', 'h.'),
+        ), tempo=150)
 
-
-
-    # def test_play_song(self):
-    #     spkr = Sound()
-    #     spkr.play_song((
-    #         ('D4', 'e3'),  # intro anacrouse
-    #         ('D4', 'e3'),
-    #         ('D4', 'e3'),
-    #         ('G4', 'h'),  # meas 1
-    #         ('D5', 'h'),
-    #         ('C5', 'e3'),  # meas 2
-    #         ('B4', 'e3'),
-    #         ('A4', 'e3'),
-    #         ('G5', 'h'),
-    #         ('D5', 'q'),
-    #         ('C5', 'e3'),  # meas 3
-    #         ('B4', 'e3'),
-    #         ('A4', 'e3'),
-    #         ('G5', 'h'),
-    #         ('D5', 'q'),
-    #         ('C5', 'e3'),  # meas 4
-    #         ('B4', 'e3'),
-    #         ('C5', 'e3'),
-    #         ('A4', 'h.'),
-    #     ), tempo=150)
-    #
     def test_play_file(self):
         spkr = Sound()
         spkr.play_file('inputFiles/bark.wav', play_type=0)
-
-
 
         with self.assertRaises(ValueError) as cm:
             spkr.play_file('file/that/does/not/exist.wav', play_type=0)
@@ -87,12 +83,11 @@ class SoundTest(unittest.TestCase):
 
         self.assertEqual(cm.exception.args[0], 'invalid sound file (inputFiles/bark), only .wav files are supported')
 
-    #
-    # def test_speak(self):
-    #     spkr = Sound()
-    #     spkr.speak("test test test test test", volume=100, play_type=1)
-    #     spkr.play_note("C4", 5)
-    #     spkr.speak("kekeroni", volume=100, play_type=0)
+    def test_speak(self):
+        spkr = Sound()
+        spkr.speak("test test test test test", volume=100, play_type=1)
+        spkr.play_note("C4", 2)
+        spkr.speak("kekeroni", volume=100, play_type=0)
 
 
 if __name__ == '__main__':
