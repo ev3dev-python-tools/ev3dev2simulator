32fe755d3c0274b3639fe629b8190f489ac7d5d4
Niels Okker <info@nielsokker.nl>
2020-04-15 14:35:38 +0200

    Added tests for robot simulator

--- tests/ev3dev2/simulator/state/test_RobotSimulator.py
+++ tests/ev3dev2/simulator/state/test_RobotSimulator.py
@@ -7,19 +7,22 @@ from tests.ev3dev2.simulator.state.test_RobotState import TestRobotState
 
 
 class TestRobotSimulator(unittest.TestCase):
-    def test_constructor(self):
+    def test_constructor_and_reset(self):
         conf = TestRobotState.default_config()
         state = RobotState(conf)
         state.setup_pymunk_shapes(1)
         sim = RobotSimulator(state)
         sim._sync_physics_sprites = MagicMock()
         sim.update()
-        self.assertEqual(True, True)
+
+        sim._sync_physics_sprites.assert_called_once()
+        sim.should_reset = True
+        self.assertEqual(sim.should_reset, True)
         sim.reset()
+        self.assertEqual(sim.should_reset, False)
         sim.release_locks()
 
-
-    def test_get_value(self):
+    def test_get_value_and_locks(self):
         conf = TestRobotState.default_config()
         state = RobotState(conf)
         state.setup_pymunk_shapes(1)
@@ -27,6 +30,9 @@ class TestRobotSimulator(unittest.TestCase):
         sim._sync_physics_sprites = MagicMock()
         val = sim.get_value((0, 'ev3-ports:in4'))
         self.assertEqual(val, 2550)
+        self.assertEqual(sim.locks[(0, 'ev3-ports:in4')].locked(), True)
+        sim.update()
+        self.assertEqual(sim.locks[(0, 'ev3-ports:in4')].locked(), False)
 
 
 if __name__ == '__main__':
